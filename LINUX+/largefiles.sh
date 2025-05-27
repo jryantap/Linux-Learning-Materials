22edd00b9cc154716c522054ebafa3d31a1eef83
Jherico-Ryan Tapang <jhericot@gmail.com>
2025-05-27 19:27:05 -0400

    Update largefiles.sh

--- LINUX+/largefiles.sh
+++ LINUX+/largefiles.sh
@@ -3,4 +3,6 @@
 #-a = include both files and directories in the output. 
 #-h = display sizes in readable format
 #/home = directory to analyze.
+#the script lists the 10 largest files or directories under /home, sort by size (largest first)
+
 du -ah /home | sort -rh | head - n10
