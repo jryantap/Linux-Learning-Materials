c52db4ffde372e0800d0bef43ec897d98ba9853d
Jherico-Ryan Tapang <jherico.tapang@outlook.com>
2025-05-29 05:53:03 -0400

    Update uptimeload.sh

--- LINUX+/Bash Scipts/uptimeload.sh
+++ LINUX+/Bash Scipts/uptimeload.sh
@@ -3,3 +3,5 @@
 echo "System Uptime: $(uptime -p)"
 #use awk -F as delimiters to make readable
 echo "Load Average: $(uptime | awk -F 'load average:' '{ print $2}')"
+#shows load average over the last 1, 5, and 15 mins for the CPU.
+
