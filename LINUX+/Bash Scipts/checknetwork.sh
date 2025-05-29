861f88b53196a178806dac4df0e1c7dec59275b6
Jherico-Ryan Tapang <jherico.tapang@outlook.com>
2025-05-29 05:56:47 -0400

    add comments

--- LINUX+/Bash Scipts/checknetwork.sh
+++ LINUX+/Bash Scipts/checknetwork.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-ping -c 8.8.8.8 > /dev/null 2>&1
 
+#-c send 1 packet only instead of continuous ping
+# 2>%1 redirect output error
+# check if network is reachable. 
+ping -c 8.8.8.8 > /dev/null 2>&1
 if[$? -eq 0]; then echo "network is up"
 else echo "network is down"
 fi
--- LINUX+/Bash Scipts/process.sh
+++ LINUX+/Bash Scipts/process.sh
@@ -1,4 +1,5 @@
 #!/user/bin/env bash
+#this could have been done line-by-line but it's all printouts of a user current process list
 echo "Hello $USER"
 echo "$USER current processes"
 echo "running processes lIst"
