9fc424e66227183c1d0512be6a30d005e9ef7a25
Jherico-Ryan Tapang <jhericot@gmail.com>
2026-09-25 07:16:18 -0400

    Create reviewLinux.sh
    Add reviewLinux.sh script for network troubleshooting

---
+++ LINUX+/Bash Scipts/reviewLinux.sh
@@ -0,0 +1,9 @@
+# Suppose the out stops immediately after: 
+#1: 192.168.1.1
+#2: no reply
+#3: no reply
+
+# "no reply" lines do not indicate failure. So, use tracepath since it check individual hops. 
+
+tracepath example.com
+
